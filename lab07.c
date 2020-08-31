53f043308b042f2b99cad395350f26adc6e5a116
troywein <45960380+troywein@users.noreply.github.com>
2020-08-31 14:39:41 -0600

    Initial commit

---
+++ lab07.c
@@ -0,0 +1,29 @@
+/** lab07.c
+* ===========================================================
+* Name: first last
+* Section: 
+* Project: Lab07
+* Purpose: Verify install of CS210 software.
+* Documentation:  N/A
+* ===========================================================
+*/
+#include <stdio.h>
+#include "myFuncts.h"   // function prototypes we create declared
+                        // in this file
+
+int main() {
+    printf("This short program validates install of CS210 software\n");
+    printf("and correct setup and configuration of VSC with GitHub\n");
+    printf("Classroom.\n");
+
+
+    // TODO 1.0
+    // Call cubeIt() with a value of 3 and print the result
+
+
+    // The following line prevents the debugging/run window
+    // from immediately closeing.  You will need to hit enter
+    // to close the terminal/cmd window
+    char c = '\0';scanf("%c", &c);
+    return 0;
+}
